2669bb0cc437b2eecc0bb6307ebe06819f2e58f8
Andris Borbás <andris.borbas@gmail.com>
2022-01-19 08:24:55 +0100

    Add stars

--- src/components/teaser/Teaser.tsx
+++ src/components/teaser/Teaser.tsx
@@ -10,9 +10,10 @@ export function Teaser() {
 				<h1 className="text-2xl font-bold text-center sm:text-4xl lg:text-6xl">
 					XIX. Simonyi Konferencia
 				</h1>
-				<hr className="mt-2 mb-1 w-full border-b border-gray-700 sm:mt-6 sm:mb-5" />
+				<hr className="mt-2 mb-1 w-full border-t-2 border-gray-700 sm:mt-6 sm:mb-5" />
 				<div className="flex flex-row justify-between w-full text-lg sm:text-xl lg:text-3xl">
-					<h2>2022.04.27.</h2><h3>BME</h3>
+					<h2>2022.04.27.</h2>
+					<h3>BME</h3>
 				</div>
 			</div>
 		</div>
