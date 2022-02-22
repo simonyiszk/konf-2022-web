75a79bca808ebd240ad7b8b2cbfd9d296860f99c
Andris Borbás <andris.borbas@gmail.com>
2022-02-22 22:40:16 +0100

    Add facebook event link

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -86,9 +86,9 @@ export function Hero() {
 					</div>
 				</div>
 				<div className="flex flex-row justify-evenly mt-4 w-full">
-					{/* <motion.a
+					<motion.a
 						className="block relative p-2 mx-4 hover:brightness-75"
-						href="https://facebook.com/schdesignbme"
+						href="https://fb.me/e/myxPUe7JM"
 						target="_blank"
 						rel="noopener"
 						style={{ willChange: "left, opacity" }}
@@ -97,7 +97,7 @@ export function Hero() {
 						initial={{ opacity: 0, left: -50 }}
 					>
 						<BsFacebook className="w-8 h-auto sm:w-10 lg:w-12" />
-					</motion.a> */}
+					</motion.a>
 					<motion.a
 						className="block relative p-2 mx-4 hover:brightness-75"
 						href="https://instagram.com/simonyikonf"
