ecdf9ecf7e4389df44dee8af43ad77349e23ec78
Andris Borbás <andris.borbas@gmail.com>
2022-04-18 12:08:25 +0200

    Disable presentation query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,8 +13,8 @@ import { SponsorSection } from "@/components/sponsors/SponsorSection";
 
 type IndexProps = {
 	data: {
-		presentations: GatsbyTypes.ContentfulPresentationConnection;
-		breaks: GatsbyTypes.ContentfulBreakConnection;
+		// presentations: GatsbyTypes.ContentfulPresentationConnection;
+		// breaks: GatsbyTypes.ContentfulBreakConnection;
 		main?: GatsbyTypes.ContentfulParagraph;
 		gallery?: GatsbyTypes.ContentfulGalleryImages;
 		organizers: GatsbyTypes.ContentfulOrganizerConnection;
@@ -26,8 +26,8 @@ type IndexProps = {
 
 export default function IndexPage({ data }: IndexProps) {
 	const {
-		presentations,
-		breaks,
+		// presentations,
+		// breaks,
 		main,
 		gallery,
 		organizers,
@@ -60,51 +60,51 @@ export default function IndexPage({ data }: IndexProps) {
 
 export const query = graphql`
 	query IndexQuery {
-		presentations: allContentfulPresentation {
-			nodes {
-				room
-				title
-				name
-				profession
-				startDate
-				endDate
-				image {
-					gatsbyImageData
-				}
-				description {
-					childMdx {
-						body
-					}
-				}
-				videoLink
-				sys {
-					contentType {
-						sys {
-							id
-						}
-					}
-				}
-			}
-		}
-		breaks: allContentfulBreak {
-			nodes {
-				startDate
-				endDate
-				room
-				text {
-					childMdx {
-						body
-					}
-				}
-				sys {
-					contentType {
-						sys {
-							id
-						}
-					}
-				}
-			}
-		}
+		# presentations: allContentfulPresentation {
+		# 	nodes {
+		# 		room
+		# 		title
+		# 		name
+		# 		profession
+		# 		startDate
+		# 		endDate
+		# 		image {
+		# 			gatsbyImageData
+		# 		}
+		# 		description {
+		# 			childMdx {
+		# 				body
+		# 			}
+		# 		}
+		# 		videoLink
+		# 		sys {
+		# 			contentType {
+		# 				sys {
+		# 					id
+		# 				}
+		# 			}
+		# 		}
+		# 	}
+		# }
+		# breaks: allContentfulBreak {
+		# 	nodes {
+		# 		startDate
+		# 		endDate
+		# 		room
+		# 		text {
+		# 			childMdx {
+		# 				body
+		# 			}
+		# 		}
+		# 		sys {
+		# 			contentType {
+		# 				sys {
+		# 					id
+		# 				}
+		# 			}
+		# 		}
+		# 	}
+		# }
 		main: contentfulParagraph(name: { eq: "Main" }) {
 			name
 			content {
