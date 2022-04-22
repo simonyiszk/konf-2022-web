9c316ffa159656aebe1fde6f6fb2b725f28e9bde
Andris Borbás <andris.borbas@gmail.com>
2022-04-22 04:11:00 +0200

    3d not working

--- src/components/stars/Stars.tsx
+++ src/components/stars/Stars.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import * as React from "react";
 
 import * as styles from "./Stars.module.scss";
@@ -5,7 +6,12 @@ import * as styles from "./Stars.module.scss";
 export function Stars() {
 	return (
 		<div className="sticky top-0 w-full select-none" aria-hidden>
-			<div className="overflow-hidden absolute mt-[-50vh] w-full h-[2000px] text-primary-200">
+			<div
+				className={clsx(
+					styles.sky,
+					"overflow-hidden absolute mt-[-50vh] w-full h-[2000px] text-primary-200",
+				)}
+			>
 				<div className={styles.stars} />
 				<div className={styles.stars2} />
 				<div className={styles.stars3} />
