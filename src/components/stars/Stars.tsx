18cf43aaa47b4d66e5626513bf4b2906fcc06e07
Andris Borbás <andris.borbas@gmail.com>
2022-04-23 12:24:23 +0200

    Make stars click through

--- src/components/stars/Stars.tsx
+++ src/components/stars/Stars.tsx
@@ -4,7 +4,10 @@ import * as styles from "./Stars.module.scss";
 
 export function Stars() {
 	return (
-		<div className="sticky top-0 w-full select-none" aria-hidden>
+		<div
+			className="sticky top-0 w-full pointer-events-none select-none"
+			aria-hidden
+		>
 			<div className="overflow-hidden absolute mt-[-50vh] w-full h-[2000px] text-primary-200">
 				<div className={styles.stars} />
 				<div className={styles.stars2} />
